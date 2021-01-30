4abbbbd1ced17c5bc0002520648078f65607c143
Javier Rodriguez Valdez <javierrv@MacBook-Pro-de-Javier.local>
2021-01-30 11:52:57 -0600

    adapt index to accept card component

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Card from './components/card/Card';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Card value="1"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
